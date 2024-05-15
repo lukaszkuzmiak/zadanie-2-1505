54d95d7ae598d4619a13b4c820a908341fbfbcb6
lukaszkuzmiak <162459802+lukaszkuzmiak@users.noreply.github.com>
2024-05-15 13:18:26 +0000

    5.4

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -2,12 +2,13 @@ public class Student {
   private String Name;
   private String Surname;
   private int Age;
+  private String Datauro;
 
-  public Student(String name, String surname, int age) {
+  public Student(String name, String surname, int age, String datauro) {
     Name = name;
     Surname = surname;
     Age = age;
-
+    Datauro = datauro;
   }
 
   public String GetName() {
@@ -22,15 +23,20 @@ public class Student {
     return Age;
   }
 
+  public String GetDatauro() {
+    return Datauro;
+  }
+
   public String ToString() {
-    return Name + " " + Surname + " " + Integer.toString(Age);
+    return Name + " " + Surname + " " + Integer.toString(Age) + " " + Datauro;
   }
 
 
   public static Student Parse(String str) {
     String[] data = str.split(" ");
-    if (data.length != 3)
-      return new Student("Parse Error", "Error", -1);
-    return new Student(data[0], data[1], Integer.parseInt(data[2]));
+    if (data.length != 4)
+      return new Student("Parse Error", "Error", -1, "Error");
+    return new Student(data[0], data[1], Integer.parseInt(data[2]), data[3]);
   }
+}
 }
